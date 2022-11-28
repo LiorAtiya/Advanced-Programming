dcb646c8de6cec1d3d9082e356b3856d6a4a0f74
Lior <lior4007@gmail.com>
2022-11-28 17:20:56 +0200

    Added threadpool

--- HW1_Threading/main.c
+++ HW1_Threading/main.c
@@ -7,57 +7,143 @@
 #define BUFFERSIZE 1024
 #define THREAD_NUM 4
 
-//global variables
+// global variables
 int key;
-char* flag;
+char *flag;
 
-void* encryptAndDecrypt(){
-	//Get data from file
-	char buffer[BUFFERSIZE];
-	fgets(buffer, BUFFERSIZE,stdin);
-	
-	//Check type of flag
-	if(strcmp(flag,"-e") == 0){
-		encrypt(buffer,key);
-		printf("%s",buffer);
-	}else if(strcmp(flag,"-d") == 0){
-		decrypt(buffer,key);
-		printf("%s",buffer);
-	}else{
-		printf("Error flag: Enter -e/-d\n");
-		exit(0);
+// void *encryptAndDecrypt()
+// {
+// 	// Get data from file
+// 	char buffer[BUFFERSIZE];
+// 	fgets(buffer, BUFFERSIZE, stdin);
+
+// 	// //Check type of flag
+// 	// if(strcmp(flag,"-e") == 0){
+// 	// 	encrypt(buffer,key);
+// 	// 	printf("%s",buffer);
+// 	// }else if(strcmp(flag,"-d") == 0){
+// 	// 	decrypt(buffer,key);
+// 	// 	printf("%s",buffer);
+// 	// }else{
+// 	// 	printf("Error flag: Enter -e/-d\n");
+// 	// 	exit(0);
+// 	// }
+// }
+
+typedef struct Task
+{
+	char buffer[5];
+} Task;
+
+Task taskQueue[256];
+int taskCount = 0;
+
+pthread_mutex_t mutexQueue;
+pthread_cond_t condQueue;
+
+void executeTask(Task *task)
+{
+	// Check type of flag
+	if (strcmp(flag, "-e") == 0)
+	{
+		encrypt(task->buffer, key);
+		printf("%s", task->buffer);
+	}
+	else if (strcmp(flag, "-d") == 0)
+	{
+		decrypt(task->buffer, key);
+		printf("%s", task->buffer);
+	}
+}
+
+void submitTask(Task task)
+{
+	pthread_mutex_lock(&mutexQueue);
+	taskQueue[taskCount] = task;
+	taskCount++;
+	pthread_mutex_unlock(&mutexQueue);
+	pthread_cond_signal(&condQueue);
+}
+void *startThread(void *args)
+{
+	while (1)
+	{
+		Task task;
+
+		pthread_mutex_lock(&mutexQueue);
+		while (taskCount == 0)
+		{
+			pthread_cond_wait(&condQueue, &mutexQueue);
+		}
+
+		task = taskQueue[0];
+		int i;
+		for (i = 0; i < taskCount - 1; i++)
+		{
+			taskQueue[i] = taskQueue[i + 1];
+		}
+		taskCount--;
+
+		pthread_mutex_unlock(&mutexQueue);
+
+		executeTask(&task);
 	}
 }
 
 int main(int argc, char *argv[])
-{	
-
-	//Check valid input
-	if(argc != 3){
+{
+	// Check valid input
+	if (argc != 3)
+	{
 		printf("Error args: Need just 2 arguments - key, flag\n");
 		exit(0);
 	}
 
-	//Get data from input args
+	// Get data from input args
 	key = atoi(argv[1]);
 	flag = argv[2];
 
+	if (strcmp(flag, "-e") != 0 && strcmp(flag, "-d") != 0)
+	{
+		printf("Error flag: Enter -e/-d\n");
+		exit(0);
+	}
+
+	char buffer[5]; // Buffer to store data
+
+	while (fgets(buffer, sizeof(buffer), stdin) != NULL)
+	{
+		Task newTask;
+		memcpy(&newTask, buffer, sizeof(buffer));
+		submitTask(newTask);
+	}
+
 	pthread_t threads[THREAD_NUM];
+	pthread_mutex_init(&mutexQueue, NULL);
+	pthread_cond_init(&condQueue, NULL);
 	int i;
-	//Create threads
-	for(i = 0 ; i < THREAD_NUM ; i++){
-		if(pthread_create(threads + i,NULL,&encryptAndDecrypt, NULL) != 0){
+	// Create threads
+	for (i = 0; i < THREAD_NUM; i++)
+	{
+		if (pthread_create(&threads[i], NULL, &startThread, NULL) != 0)
+		{
 			perror("Failed to create the thread");
 		}
 	}
 
-	//Join threads
-	for(i = 0 ; i < THREAD_NUM ; i++){
-		if(pthread_join(threads[i], NULL) != 0){
+	// Join threads
+	for (i = 0; i < THREAD_NUM; i++)
+	{
+		if (pthread_join(threads[i], NULL) != 0)
+		{
 			perror("Failed to join the thread");
 		}
 	}
 
+	pthread_cond_destroy(&condQueue);
+	pthread_mutex_destroy(&mutexQueue);
+
+	//============================================================
 	// //Check type of flag
 	// if(strcmp(flag,"-e") == 0){
 	// 	encrypt(buffer,key);
